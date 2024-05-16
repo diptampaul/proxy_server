f610a4fd698e25672c3693c02ecec0dde2ef0fcc
Diptam Paul <43668430+diptampaul@users.noreply.github.com>
2024-05-16 21:36:52 +0530

    Update proxy_server.py

--- proxy_server.py
+++ proxy_server.py
@@ -25,6 +25,29 @@ def proxy():
         # Handle any errors that occur during the request
         print(e)
         return str(e), 500
+    
+@app.route('/proxy-stream', methods=['POST'])
+def proxy():
+
+    data = request.get_json()
+    url = data.get('url')  
+    headers = data.get('headers')
+
+    if not url:
+        return "No URL provided", 400
+    
+    try:
+        # Send a GET request to the URL
+        response = requests.get(url,headers=headers,timeout=30,stream=True)
+        print(url)
+        # print(response.text)
+        
+        # Return the content of the response
+        return jsonify({"content": response.content, "status_code": response.status_code})
+    except requests.exceptions.RequestException as e:
+        # Handle any errors that occur during the request
+        print(e)
+        return str(e), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
