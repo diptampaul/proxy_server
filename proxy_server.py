ce88e8256ba490cef4e01d93b426d97ad00fbddc
Diptam Paul <43668430+diptampaul@users.noreply.github.com>
2024-09-02 19:01:12 +0530

    Get reponse content added

--- proxy_server.py
+++ proxy_server.py
@@ -99,6 +99,30 @@ def proxy_stream():
         # Handle any errors that occur during the request
         print(e)
         return str(e), 500
+    
+
+@app.route('/proxy-content', methods=['POST'])
+def proxy_content():
+
+    data = request.get_json()
+    url = data.get('url')  
+    headers = data.get('headers')
+
+    if not url:
+        return "No URL provided", 400
+    
+    try:
+        # Send a GET request to the URL
+        response = requests.get(url,headers=headers,timeout=30)
+        print(url)
+        # print(response.text)
+        
+        # Return the content of the response
+        return jsonify({"content": response.content, "status_code": response.status_code})
+    except requests.exceptions.RequestException as e:
+        # Handle any errors that occur during the request
+        print(e)
+        return str(e), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
